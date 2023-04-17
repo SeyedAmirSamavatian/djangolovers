2609651cbd5f5be5f5400e5a6071fdcfa3396be1
Seyed_Amir <mr.smvtn@gmail.com>
2023-04-17 13:28:25 +0330

    Part 3

--- djangolovers/urls.py
+++ djangolovers/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import welcome
+from .views import welcome , base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome , name='welcome'),
-]
-
+    path('base/', base , name='base'),  #movaghati
+] 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- djangolovers/views.py
+++ djangolovers/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 
+
 def welcome(request):
 	return render(request,'index.html')
+
+
+
+
+def base(request):
+	return render(request,'base.html')
